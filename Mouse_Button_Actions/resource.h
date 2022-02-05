b53b1e0c046cc009530493c65d1c2aa9ebad56e0
Kunihiro Ando <kxkx5150@users.noreply.github.com>
2022-02-05 18:22:54 +0900

    v1.1

--- Mouse_Button_Actions/resource.h
+++ Mouse_Button_Actions/resource.h
@@ -2,21 +2,19 @@
 // Microsoft Visual C++ generated include file.
 // Used by Mouse_Button_Actions.rc
 //
-#define IDC_MYICON                      2
 #define IDD_MOUSEHOOKAPP_DIALOG         102
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
 #define IDM_EXIT                        105
-#define IDI_MOUSEHOOKAPP                107
-#define IDI_SMALL                       108
 #define IDC_MOUSEHOOKAPP                109
 #define IDR_MAINFRAME                   128
+#define IDC_MYICON                      129
 #define IDI_ICON1                       129
 #define IDD_DIALOG1                     130
 #define ID_MENU_STARTMOUSEHOOK          133
 #define ID_MENU_ENDMOUSEHOOK            134
-#define ID_MENU_STARTUP					135
+#define ID_MENU_STARTUP                 135
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
